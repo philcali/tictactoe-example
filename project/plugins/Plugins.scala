7e2e23d0a883d1d5568e319af53590fdba0e2929
Philip Cali <philip.cali@gmail.com>
2011-04-20 23:34:02 -0500

    Changed example app to use the latest build.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,5 +1,5 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0"
+  val lwjglPlugin = "com.github.philcali" % "sbt-lwjgl-plugin" % "2.0.5"
 }
